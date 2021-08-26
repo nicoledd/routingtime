60d49a2dc9a6ae8e4fced4fd764b67c5bac7c651
Nicole <invalid_email>
2021-08-25 20:13:35 -0600

    lalalla

--- bashscript.sh
+++ bashscript.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=7:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4G
-#SBATCH --array=0-8399
+#SBATCH --array=0-6299
 #SBATCH --exclude=openlab[30-33]
 # Mail me when starting and stopping
 #SBATCH --mail-type=END,FAIL
