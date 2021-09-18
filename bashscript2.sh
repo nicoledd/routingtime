dc46c146134544bb9412d6c68278268b201cce15
Nicole <invalid_email>
2021-09-18 11:33:46 -0600

    edit

--- bashscript2.sh
+++ bashscript2.sh
@@ -18,6 +18,4 @@ module load Python3
 
 python3 -m pip install --user networkx
 
-python3 -m pip install --user ConfigModel_MCMC
-
 python3 main2.py -id $SLURM_ARRAY_TASK_ID || echo "TERRIBLE_ERROR_HAPPENED"
--- main2.py
+++ main2.py
@@ -49,6 +49,6 @@ args = parser.parse_args()
 files = ['./facebook100txt/UCSB37.txt', './facebook100txt/Wake73.txt', './facebook100txt/Wesleyan43.txt', './facebook100txt/Auburn71.txt', './facebook100txt/Princeton12.txt', './facebook100txt/USFCA72.txt', './facebook100txt/Cal65.txt', './facebook100txt/UC61.txt', './facebook100txt/UC64.txt', './facebook100txt/Rutgers89.txt', './facebook100txt/Haverford76.txt', './facebook100txt/Carnegie49.txt', './facebook100txt/Mississippi66.txt', './facebook100txt/Vanderbilt48.txt', './facebook100txt/Santa74.txt', './facebook100txt/JMU79.txt', './facebook100txt/UPenn7.txt', './facebook100txt/UNC28.txt', './facebook100txt/NYU9.txt', './facebook100txt/Brown11.txt', './facebook100txt/USF51.txt', './facebook100txt/Brandeis99.txt', './facebook100txt/USC35.txt', './facebook100txt/Lehigh96.txt', './facebook100txt/Columbia2.txt']
 
 
-x, p = getDensity(file[args.id%25])
+x, p = getDensity(files[args.id%25])
 y = getRandGraphGeo(x, p)
 print(x, y)
